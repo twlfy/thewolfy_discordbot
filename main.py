8288395c15b28851b3b5289f43078a08d96fb2ef
TheWolfy707 <79784107+TheWolfy707@users.noreply.github.com>
2021-06-16 23:44:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import discord
 from discord.ext import commands
+from discord.ext.commands.errors import MissingPermissions
 
 client = commands.Bot(command_prefix = '!')
 @client.event
@@ -10,6 +11,26 @@ async def on_ready():
 
 # ==== Bot Commands ==== #
 
+@client.command() #=== !hellobot ===
+async def hellobot(ctx): #ctx = context parameter
+     await ctx.send("Howdy, userino ! I'm TheWølfy bot and right now i'm online ! If you want help please type '!helpme'. Thanks ^_^")
+
+@client.command() #=== !helpme ===
+async def helpme(ctx):
+    await ctx.send("""Glad to help you, userino ! The following are the commands and what can i do !: 
+    !helpme - You already typed the command, lul. That's a command listing all the commands :>
+    !hellobot - A hello message just to check if i'm on duty xd""")
+
+@client.command() #=== !clear ===
+@commands.has_permissions(administrator=True) # Command Permissions to administrator
+async def clear(ctx, amount=None): #amount = amount of messages to delete
+    await ctx.channel.purge(limit=amount) #ctx.channel = channel where is executed calling the purge
+    await ctx.send("Deleted. Oopsie....i've typed in :\ might delete later")
+
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("Oopsie... You don't have admin permissions")
 
 # ==== Bot Client Key ==== #
 client.run("ODU0MzA1Nzc2NjUwNDIwMjI0.YMiAQQ.Wef2LP-tMqyds7tAuafaTIHcdvo")
