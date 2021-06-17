6b04a9568097788838888de3d0121205c9adf839
TheWølfy <79784107+TheWolfy707@users.noreply.github.com>
2021-06-17 09:43:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def helpme(ctx):
     !hellobot - A hello message just to check if i'm on duty xd""")
 
 @client.command() #=== !clear ===
-@commands.has_role("Moderator")  # check the user role
+@commands.has_role("Moderator")  #check the user role
 async def clear(ctx, amount = 0):
     await ctx.channel.purge(limit=amount+1)  #ctx.channel = channel where is executed calling the purge
     await ctx.send("Deleted. Oopsie....i've typed in :\ might delete later")
