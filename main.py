921fe6612eef052b39b367ed80c7b87f0a97ea78
TheWølfy <79784107+twlfy@users.noreply.github.com>
2021-08-23 01:29:43 -0700

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# ==== Bot Connextion ==== #
+# ==== Libraries ==== #
 
 from typing import AsyncContextManager, AsyncIterator
 import discord
@@ -16,7 +16,16 @@ from discord import FFmpegPCMAudio
 from os import system
 import asyncio
 
+# ======= Role server setup ======= #
+#
+#
+#
+#
+#
+#
 
+# ======= Settings ======= #
+# ==== Audio Settings ==== #
 ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -28,6 +37,8 @@ ydl_opts = {
 
 def endSong(guild, path):
     os.remove(path)                                   
+
+# ==== Events ==== #
 
 client = commands.Bot(command_prefix = '.')
 @client.event
@@ -49,7 +60,7 @@ async def on_guild_join(guild):
 async def hellobot(ctx): #ctx = context parameter
      await ctx.send("Howdy, userino ! I'm TheWølfy bot and right now i'm online ! If you want help please type '.helpme'. Thanks ^_^")
 
-@client.command() #=== !helpme ===
+@client.command() #=== .helpme ===
 async def helpme(ctx):
     await ctx.send("""Glad to help you, userino ! The following are the commands and what can i do !: 
     .helpme - You already typed the command, lul. That's a command listing all the commands :>
@@ -65,19 +76,19 @@ async def helpme(ctx):
     """)
 
 @client.command()
-async def onlinecheck(ctx):
+async def onlinecheck(ctx): #=== .onlinecheck ===
     await ctx.send("""Status: Online
     
     Yes ! I'm online, but only when my master is online too. If you want to manage this server 24/7, pay fucking 4 dollars for a hosting service. xoxo""")
 
-@client.command() #=== !clear ===
+@client.command() #=== .clear ===
 @commands.has_role("tot eu dar cu rosu ca-mi plc")
 async def clear(ctx, amount = 0):
     await ctx.channel.purge(limit=amount+1)  #ctx.channel = channel where is executed calling the purge
     await ctx.send("Deleted. Oopsie....i've typed in :\ might delete later")
 
 
-@client.command() #=== !kick ===
+@client.command() #=== .kick ===
 @commands.has_role("Chad aimer")
 async def kick(ctx, member : discord.Member, *, reason=None): #saying member to read discord's member function
                                                               # * = Offer space after every argument written by the moderator / administrator
@@ -88,7 +99,7 @@ async def kick(ctx, member : discord.Member, *, reason=None): #saying member to 
     await member.kick(reason=reason)
     await ctx.send(f"{member} has been kicked :^ (Reason: {reason})")
         
-@client.command() #=== !ban ===
+@client.command() #=== .ban ===
 @commands.has_role("Chad aimer")
 async def ban(ctx, member : discord.Member, *, reason=None): #saying member to read discord's member function
     if ctx.author.top_role <= member.top_role:
@@ -112,7 +123,7 @@ async def clear_error(ctx,error):
     if isinstance(error, (commands.MissingRole, commands.MissingAnyRole)):
         await ctx.send("You have to own a role upper than 'Unicorn Intergalactic' to perform a clear chat :>")
 
-@client.command() #=== !about ===
+@client.command() #=== .about ===
 async def about(ctx):
     await ctx.send(f"""Hi {ctx.author.mention} ! I'm TheWølfy bot, the same nickname as my creator's one. 
     I am here to help communities develop their own administration stuff and also maybe have fun together! 
@@ -123,8 +134,7 @@ async def about(ctx):
     Created: June 2021
     Version: 1.1T (R - Official Release | T - Beta version test)""")
 
-#=== music bot ===
-@client.command(pass_context=True)
+@client.command(pass_context=True) #=== .play ===
 async def play(ctx, url):
     if not ctx.message.author.voice:
         await ctx.send('You are not connected to a voice channel :(')
