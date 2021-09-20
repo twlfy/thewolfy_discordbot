9787d64d8de476017047528c5d53f8c754de7e41
TheWølfy <79784107+twlfy@users.noreply.github.com>
2021-09-20 13:30:00 -0700

    .

--- main.py
+++ main.py
@@ -164,4 +164,4 @@ async def play(ctx, url):
         print("Disconnected")
 
 # ==== Bot Client Key ==== #
-client.run("ODU0MzA1Nzc2NjUwNDIwMjI0.YMiAQQ.ihbH7wLifEN3Y97b8Qxfa6cCJ_w")
+client.run("ODU0MzA1Nzc2NjUwNDIwMjI0.YMiAQQ.0awbU-lJfm2ISuc8uPEBQrNIjeM")
